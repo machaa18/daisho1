8dafa339af0d8f5a83eac5f6c5682d7d2b034c37
machaa18 <honyo.almond1008@gmail.com>
2025-10-16 15:22:07 +0900

    20251016V01

---
+++ daisho-day1/script.js
@@ -0,0 +1,13 @@
+const helloBtn=document.getElementById("hellobBtn");
+const message=document.getElementById("message");
+
+helloBtn.addEventListener("click",()=>{
+    const hour=newData().getHours();
+    let greeting="こんにちは！";
+    if(hour<12){
+        greeting="おはようございます！";
+    }else if(hour>=18){
+        greeting="こんばんは！";
+    }
+    message.textContent='${greeting}GitHub Pages の公開に成功しました！';
+});
