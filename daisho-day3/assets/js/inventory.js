272571df03b807a711d78d5968c3080d75aa2ec2
machaa18 <honyo.almond1008@gmail.com>
2025-12-11 14:38:50 +0900

    macha

--- daisho-day3/assets/js/inventory.js
+++ daisho-day3/assets/js/inventory.js
@@ -4,8 +4,8 @@
 // ------------------------------
 const items = [
     { id: "D001", name: "オレンジジュース", price: 150, stock: 20, minStock: 5},
-    { id: "D002", name: "コーラ", price: 150, stock: 12, minStock: 5},
-    { id: "D003", name: "お茶", price: 120, stock: 30, minStock: 10}
+    { id: "D002", name: "コーラ",           price: 150, stock: 12, minStock: 5},
+    { id: "D003", name: "お茶",             price: 120, stock: 30, minStock: 10}
 ];
 
 // HTML で <tbody id="item-list"> と書いた部分を JavaScript から取得する
